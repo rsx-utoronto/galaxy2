089c97a44024e2c708fe1b264bdf5c0808583ff6
RSX <contact@rsx-utoronto.com>
2016-03-19 17:08:33 -0400

    Began testing serial connection. Information is correct, but ser.inWaiting always returns 0.

---
+++ ground/PacketSerial.py
@@ -0,0 +1,46 @@
+import serial
+from collections import deque
+
+class PacketSerial():
+	""" 
+	Really hack class to write 4 serial packets at once.
+	Note that the packets are not checked for orer. 
+	Read and write both take 4 bits at once
+	"""
+	def __init__(self, serial_connection):	
+		self.ser = serial_connection
+		self.d = deque(["a", "a", "a", "a"]) 
+
+	def read(self):
+		"returns byte[4] or None"
+		if self.ser.inWaiting() < 4:  # in newer versions of pyserial, it's in_waiting (not a function, no brackets)
+		    print("Not enough bytes", self.ser.inWaiting()) 
+		    return None
+		
+		for i in range(4):
+			byte = self.ser.read()	
+			
+			if byte is None or byte == "":
+			    return None
+
+			# check whether it's a checksum packet
+			checksum = 0
+			for i in self.d:
+				checksum ^= ord(i)
+			if checksum == byte:
+				return list(self.d)
+			else: 
+				self.d.append(byte)
+				self.d.popleft()
+
+	def write(self, bytes):
+		"takes byte[4], returns None" 
+		if len(bytes) != 4:
+			raise ValueError("Array is not 4 bytes long!")
+
+		checksum = 0
+		for i in bytes:
+			self.ser.write(i)
+			checksum ^= ord(i)
+		self.ser.write(chr(checksum))
+		
