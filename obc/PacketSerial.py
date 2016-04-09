7f142c1a0529ce4ba2d63f2e7c9d583845cf06c8
RSX <contact@rsx-utoronto.com>
2016-04-09 13:46:21 -0400

    ground station commit

--- obc/PacketSerial.py
+++ obc/PacketSerial.py
@@ -2,41 +2,39 @@ import serial
 from collections import deque
 
 class PacketSerial():
-	""" 
-	Really hack class to write 4 serial packets at once.
-	Note that the packets are not checked for orer. 
-	Read and write both take 4 bits at once
-	"""
-	def __init__(self, serial_connection):	
-		self.ser = serial_connection
-		self.d = deque(["a", "a", "a", "a"]) 
+    """ 
+    Really hack class to write 4 serial packets at once.
+    Note that the packets are not checked for orer. 
+    Read and write both take 4 bits at once
+    """
+    def __init__(self, serial_connection):    
+        self.ser = serial_connection
+        self.d = deque(["a", "a", "a", "a"]) 
 
-	def read(self):
-		"returns byte[4] or None"
-		for i in range(4):
-			byte = self.ser.read()	
-			
-			if byte is None or byte == "":
-			    return None
+    def read(self):
+        "returns byte[4] or None"
+        
+        for i in range(5):
+            byte = self.ser.read()
 
-			# check whether it's a checksum packet
-			checksum = 0
-			for i in self.d:
-				checksum ^= ord(i)
-			if checksum == byte:
-				return list(self.d)
-			else: 
-				self.d.append(byte)
-				self.d.popleft()
+            # check whether it's a checksum packet
+            checksum = 0
+            for i in self.d:
+                checksum ^= ord(i)
+            if checksum == ord(byte):
+                return list(self.d)
+            else: 
+                self.d.append(byte)
+                self.d.popleft()
 
-	def write(self, bytes):
-		"takes byte[4], returns None" 
-		if len(bytes) != 4:
-			raise ValueError("Array is not 4 bytes long!")
+    def write(self, bytes):
+        "takes byte[4], returns None" 
+        if len(bytes) != 4:
+            raise ValueError("Array is not 4 bytes long!")
 
-		checksum = 0
-		for i in bytes:
-			self.ser.write(i)
-			checksum ^= ord(i)
-		self.ser.write(chr(checksum))
-		
+        checksum = 0
+        for i in bytes:
+            self.ser.write(i)
+            checksum ^= ord(i)
+        self.ser.write(chr(checksum))
+        
